9a226aeba60d809508ecc578e4c63f343622ba1d
andreiolaru <cs@andreiolaru.ro>
2018-11-19 00:32:21 +0200

    Added SimpleLoader as a default Loader implementation.

--- src/net/xqhs/flash/core/Loader.java
+++ src/net/xqhs/flash/core/Loader.java
@@ -11,6 +11,7 @@
  ******************************************************************************/
 package net.xqhs.flash.core;
 
+import net.xqhs.flash.core.util.PlatformUtils;
 import net.xqhs.flash.core.util.TreeParameterSet;
 import net.xqhs.util.logging.Logger;
 
@@ -19,6 +20,9 @@ import net.xqhs.util.logging.Logger;
  * <p>
  * Entities are created based on a configuration that is loaded at boot time and is represented as a
  * {@link TreeParameterSet}.
+ * <p>
+ * For many types of entities the loader may not be necessary. If instantiating the class if sufficient,
+ * {@link SimpleLoader} may be used.
  * 
  * @param <T>
  *            the type of {@link Entity} instance that the loader can load.
@@ -70,4 +74,114 @@ public interface Loader<T extends Entity<?>>
 	 * @return the entity, if loading has been successful.
 	 */
 	public T load(TreeParameterSet configuration);
+	
+	/**
+	 * Simple implementation for {@link Loader}, which attempts to load the entity class by instantiating it:
+	 * <ul>
+	 * <li>using a constructor that receives a {@link TreeParameterSet} as argument, or
+	 * <li>using the default constructor, and if the instance implements {@link ConfigurableEntity}, subsequently
+	 * calling {@link ConfigurableEntity#configure}.
+	 * </ul>
+	 * <p>
+	 * The class is loaded using the classpath provided as the first value in the simple key of the given entity
+	 * configuration. If no such classpath exists, loading fails.
+	 * 
+	 * @author andreiolaru
+	 */
+	public class SimpleLoader implements Loader<Entity<?>>
+	{
+		/**
+		 * Name of the key in which the classpath should be stored.
+		 */
+		public static final String	CLASSPATH_KEY	= "classpath";
+		
+		/**
+		 * The log to use.
+		 */
+		protected Logger			log				= null;
+		
+		@Override
+		public boolean configure(TreeParameterSet configuration, Logger _log)
+		{
+			log = _log;
+			return true;
+		}
+		
+		/**
+		 * The preload method tests if the classpath is set and exists.
+		 */
+		@Override
+		public boolean preload(TreeParameterSet configuration)
+		{
+			if(!configuration.isSimple(CLASSPATH_KEY))
+			{
+				if(log != null)
+					log.le("Classpath not set in configuration (missing classpath key)");
+				return false;
+			}
+			if(!PlatformUtils.getClassFactory().canLoadClass(configuration.get(CLASSPATH_KEY)))
+			{
+				if(log != null)
+					log.le("Class cannot be loaded from given classpath []", configuration.get(CLASSPATH_KEY));
+				return false;
+			}
+			return true;
+		}
+		
+		/**
+		 * The method will attempt to instantiate a new entity, using the classpath from the {@value #CLASSPATH_KEY}
+		 * parameter in the configuration (as a simple key).
+		 * <p>
+		 * The instantiation is tried in order:
+		 * <ul>
+		 * <li>using a constructor that receives a {@link TreeParameterSet} as argument, passing the given
+		 * configuration;
+		 * <li>using the default constructor, and if the instance implements {@link ConfigurableEntity}, the
+		 * {@link ConfigurableEntity#configure} method will be called with the given configuration.
+		 * </ul>
+		 */
+		@Override
+		public Entity<?> load(TreeParameterSet configuration)
+		{
+			if(preload(configuration))
+			{
+				String classpath = configuration.get(CLASSPATH_KEY);
+				try
+				{
+					// try Constructor(configuration)
+					return (Entity<?>) PlatformUtils.getClassFactory().loadClassInstance(classpath, configuration,
+							false);
+				} catch(Exception e)
+				{
+					if(e instanceof NoSuchMethodException)
+					{// no constructor with configuration argument
+						try
+						{
+							Entity<?> loaded = (Entity<?>) PlatformUtils.getClassFactory().loadClassInstance(classpath,
+									null, true);
+							if(loaded instanceof ConfigurableEntity)
+							{// default constructor used, must try to configure
+								if(!((ConfigurableEntity<?>) loaded).configure(configuration) && log != null)
+									log.le("Entity [] loaded from [] but configuration failed.", loaded.getName(),
+											classpath);
+							}
+							else
+								log.le("Configuration not sent to entity [] loaded from []", loaded.getName(),
+										classpath);
+							return loaded;
+						} catch(Exception e1)
+						{
+							if(log != null)
+								log.le("Failed to load class [] via default constructor:", classpath,
+										PlatformUtils.printException(e));
+						}
+					}
+					if(log != null)
+						log.le("Failed to load class [] via constructor with configuration:",
+								configuration.get(CLASSPATH_KEY), PlatformUtils.printException(e));
+				}
+			}
+			return null;
+		}
+	}
 }
