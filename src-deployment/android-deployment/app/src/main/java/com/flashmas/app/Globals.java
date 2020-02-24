9bdfdf9b6fc84d9ca1455ba658521ca70c7f0946
Daniel <daniel.berbece97@gmail.com>
2020-02-24 19:22:58 +0200

    Add foreground service for node

---
+++ src-deployment/android-deployment/app/src/main/java/com/flashmas/app/Globals.java
@@ -0,0 +1,7 @@
+package com.flashmas.app;
+
+public class Globals {
+    public static final int NODE_FOREGROUND_ID = 101;
+    public static final String NODE_NOTIF_CHANNEL_ID = "node_notif_channel_id";
+
+}
--- src-deployment/android-deployment/app/src/main/java/com/flashmas/app/MainActivity.java
+++ src-deployment/android-deployment/app/src/main/java/com/flashmas/app/MainActivity.java
@@ -1,12 +1,10 @@
 package com.flashmas.app;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.flashmas.app.agents.TestAgent;
-
-import net.xqhs.flash.local.LocalSupport;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,18 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//
+//        LocalSupport pylon = new LocalSupport();
+//
+//        TestAgent one = new TestAgent("One");
+//        one.addContext(pylon.asContext());
+//        TestAgent two = new TestAgent("Two");
+//        two.addContext(pylon.asContext());
+//
+//        one.addMessagingShard(new LocalSupport.SimpleLocalMessaging());
+//        two.addMessagingShard(new LocalSupport.SimpleLocalMessaging());
+//
+//        one.start();
+//        two.start();
 
-        LocalSupport pylon = new LocalSupport();
-
-        TestAgent one = new TestAgent("One");
-        one.addContext(pylon.asContext());
-        TestAgent two = new TestAgent("Two");
-        two.addContext(pylon.asContext());
-
-        one.addMessagingShard(new LocalSupport.SimpleLocalMessaging());
-        two.addMessagingShard(new LocalSupport.SimpleLocalMessaging());
-
-        one.start();
-        two.start();
+        Intent intent = new Intent(this, NodeForegroundService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent);
+        } else {
+            startService(intent);
+        }
     }
 }
