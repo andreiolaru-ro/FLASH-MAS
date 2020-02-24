42224105b0e530f57eaff47fab89bd5590cb3b21
Daniel <daniel.berbece97@gmail.com>
2020-02-24 19:33:21 +0200

    Add start and stop buttons

--- src-deployment/android-deployment/app/src/main/java/com/flashmas/app/MainActivity.java
+++ src-deployment/android-deployment/app/src/main/java/com/flashmas/app/MainActivity.java
@@ -3,6 +3,8 @@ package com.flashmas.app;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -26,11 +28,27 @@ public class MainActivity extends AppCompatActivity {
 //        one.start();
 //        two.start();
 
-        Intent intent = new Intent(this, NodeForegroundService.class);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(intent);
-        } else {
-            startService(intent);
-        }
+        Button start = findViewById(R.id.start_node);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NodeForegroundService.class);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(intent);
+                } else {
+                    startService(intent);
+                }
+            }
+        });
+
+
+        Button stop = findViewById(R.id.stop_node);
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NodeForegroundService.class);
+                stopService(intent);
+            }
+        });
     }
 }
