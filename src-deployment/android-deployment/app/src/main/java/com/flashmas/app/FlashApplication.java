449e4e26e30d2fb5886335e42066edb68bf58847
Daniel <daniel.berbece97@gmail.com>
2020-03-02 16:56:43 +0200

    Add application class

---
+++ src-deployment/android-deployment/app/src/main/java/com/flashmas/app/FlashApplication.java
@@ -0,0 +1,11 @@
+package com.flashmas.app;
+
+import android.app.Application;
+
+public class FlashApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
