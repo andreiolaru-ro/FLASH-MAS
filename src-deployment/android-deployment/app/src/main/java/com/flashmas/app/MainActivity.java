f23a6cee94df4aea031cd24e3f84f4a9c4d8d951
Daniel <daniel.berbece97@gmail.com>
2020-03-04 23:35:14 +0200

    Add list of Agents using RecyclerView

--- src-deployment/android-deployment/app/src/main/java/com/flashmas/app/MainActivity.java
+++ src-deployment/android-deployment/app/src/main/java/com/flashmas/app/MainActivity.java
@@ -7,13 +7,35 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import net.xqhs.flash.core.agent.Agent;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    AgentsListAdapter adapter = new AgentsListAdapter(this);
+
+    Observer<List<Agent>> agentsObserver = new Observer<List<Agent>>() {
+        @Override
+        public void onChanged(List<Agent> agents) {
+            if (adapter == null) {
+                return;
+            }
+
+            adapter.updateData(agents);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO use only one button for start and stop
+        // TODO use ViewModel
 
         Button start = findViewById(R.id.start_node);
         start.setOnClickListener(new View.OnClickListener() {
@@ -37,5 +59,12 @@ public class MainActivity extends AppCompatActivity {
                 stopService(intent);
             }
         });
+
+        // Set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.agents_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        NodeForegroundService.getAgentsLiveData().observe(this, agentsObserver);
     }
 }
