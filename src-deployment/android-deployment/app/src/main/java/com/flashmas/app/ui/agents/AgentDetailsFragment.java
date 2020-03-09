1b08157fc1728b76b74f7960a43f57c54ec1d448
Daniel <daniel.berbece97@gmail.com>
2020-03-09 21:31:03 +0200

    Polish and styling

--- src-deployment/android-deployment/app/src/main/java/com/flashmas/app/ui/agents/AgentDetailsFragment.java
+++ src-deployment/android-deployment/app/src/main/java/com/flashmas/app/ui/agents/AgentDetailsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.flashmas.app.R;
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 import net.xqhs.flash.core.agent.Agent;
 
@@ -29,10 +31,19 @@ public class AgentDetailsFragment extends Fragment {
         Bundle args = getArguments();
 
         TextView nameTextView = view.findViewById(R.id.agent_name);
+        SwitchMaterial switchMaterial = view.findViewById(R.id.state_switch);
 
         if (args != null && args.get(AGENT_KEY) instanceof Agent) {
             Agent selectedAgent = (Agent) args.get(AGENT_KEY);
             nameTextView.setText(selectedAgent.getName());
+            switchMaterial.setChecked(selectedAgent.isRunning());
         }
+        switchMaterial.setClickable(false); // enable after listener implementation
+        switchMaterial.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // TODO disable/enable agent
+            }
+        });
     }
 }
