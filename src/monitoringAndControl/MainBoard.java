388fcc608942ddee8df4fefc89fc9bc7156d7b1a
nflorina <florina.nastasoiu@yahoo.com>
2020-03-14 17:37:45 +0200

    Implement functionality behind Create and Start buttons
    The main GUI has 4 buttons: "Create agents", "Start agents", "Stop agents" and "Exit". "Create agents" is used to load the deployment and create agents within a specific node (the loggingArea is populated with the specific log text), whereas "Start agents" calls start() method on every entity within that node (the logging area is updated and the list of agents from the right of the board is populated with the name of current agents).

--- src/monitoringAndControl/MainBoard.java
+++ src/monitoringAndControl/MainBoard.java
@@ -1,32 +1,75 @@
 package monitoringAndControl;
 
+import net.xqhs.flash.core.agent.Agent;
+import net.xqhs.flash.core.node.Node;
+import net.xqhs.flash.core.node.NodeLoader;
+import net.xqhs.util.logging.LoggerSimple;
+import net.xqhs.util.logging.logging.Logging;
+import net.xqhs.util.logging.wrappers.GlobalLogWrapper;
+
 import javax.swing.*;
 import java.awt.*;
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainBoard {
-    private JTextArea loggingArea;
-    private JButton createAgents;
-    private JButton startAgents;
-    private JButton stopAgents;
+    private JTextArea loggingAreaText;
+    private JButton createAgentsButton;
+    private JButton startAgentsButton;
+    private JButton stopAgentsButton;
     private JButton exitButton;
-    private JPanel panel1;
-    private JList agentsList;
+    private JPanel panel;
+    private JList<String> agentsList;
+
+    private ByteArrayOutputStream out;
+    private List<Node> nodes;
+    private List<Agent> agents;
+    private DefaultListModel<String> listModel;
+
+    private String arg;
+
+    public MainBoard(String arg) {
+        this.arg = arg;
+        listModel = new DefaultListModel<String>();
+        out = new ByteArrayOutputStream();
+        agentsList.setModel(listModel);
+
+        Logging.getMasterLogging().setLogLevel(LoggerSimple.Level.ALL);
+        GlobalLogWrapper.setLogStream(out);
+    }
 
 
-    public void addListenersToComponents() {
-        createAgents.addActionListener(actionEvent -> {
+    private void addListenersToComponents() {
+        createAgentsButton.addActionListener(actionEvent -> {
+            String[] args = arg.split(" ");
+            nodes = new NodeLoader().loadDeployment(Arrays.asList(args));
+            loggingAreaText.setText(out.toString());
         });
-        startAgents.addActionListener(actionEvent -> {
+
+        startAgentsButton.addActionListener(actionEvent -> {
+            agents = new LinkedList<>();
+            for(Node node : nodes) {
+                node.start();
+                agents.addAll(node.getAgents());
+            }
+            loggingAreaText.setText(out.toString());
+
+            for (Agent agent : agents)
+                listModel.addElement(agent.getName());
         });
-        stopAgents.addActionListener(actionEvent -> JOptionPane.showMessageDialog(null, "Hello!"));
+
+
+        stopAgentsButton.addActionListener(actionEvent -> JOptionPane.showMessageDialog(null, "Hello!"));
         exitButton.addActionListener(actionEvent -> JOptionPane.showMessageDialog(null, "Hello!"));
     }
 
 
-    private void createAndShowGUI() {
+    public void createAndShowGUI() {
         JFrame frame = new JFrame("Monitoring and control");
         addListenersToComponents();
-        frame.setContentPane(panel1);
+        frame.setContentPane(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
 
@@ -37,10 +80,6 @@ public class MainBoard {
         frame.setLocationRelativeTo(null);
 
         frame.setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> new MainBoard().createAndShowGUI());
     }
 
 }
---
+++ src/monitoringAndControlTest/MonitoringAndControlTest.java
@@ -0,0 +1,32 @@
+package monitoringAndControlTest;
+
+import monitoringAndControl.MainBoard;
+import net.xqhs.flash.FlashBoot;
+
+public class MonitoringAndControlTest {
+    /**
+     * Performs test.
+     *
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        String test_args = "";
+
+        test_args += "old-deployment/composite/basicScenario.xml";
+
+        test_args += " -package deploymentTest -loader agent:composite";
+        test_args += " -agent composite:AgentA -shard messaging -shard PingTestComponent -shard MonitoringTest";
+        test_args += " -agent composite:AgentB -shard messaging -shard PingBackTestComponent -shard MonitoringTestShard";
+
+
+        String finalTest_args = test_args;
+        javax.swing.SwingUtilities.invokeLater(() -> new MainBoard(finalTest_args).createAndShowGUI());
+    }
+}
+
+
+
+
+
+
