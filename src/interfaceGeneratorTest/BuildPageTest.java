60d8732e234558b68d1a596947f3ebea0f398236
Florin Mihalache <florinrmfm3@gmail.com>
2020-04-05 13:16:51 +0300

    Program ends when swing window closed

--- src/interfaceGeneratorTest/BuildPageTest.java
+++ src/interfaceGeneratorTest/BuildPageTest.java
@@ -1,7 +1,16 @@
 package interfaceGeneratorTest;
 
-public class BuildPageTest {
-    public static void main(String[] args) {
+import interfaceGenerator.PageBuilder;
 
+public class BuildPageTest {
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            System.err.println("No file specified.");
+            return;
+        }
+        var file = PageBuilder.buildPageFile(args[0]);
+        if (file != null) {
+            PageBuilder.buildPage(file);
+        }
     }
 }
