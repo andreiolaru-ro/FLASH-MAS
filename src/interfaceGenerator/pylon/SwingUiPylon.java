02cd28e76de2d870223dd233b641c9fcb89c588c
Florin Mihalache <florinrmfm3@gmail.com>
2020-04-05 13:24:09 +0300

    Restructured code in SwingUiPylon (more modular)

--- src/interfaceGenerator/pylon/SwingUiPylon.java
+++ src/interfaceGenerator/pylon/SwingUiPylon.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class SwingUiPylon {
-    public static HashMap<String, Component> componentMap = new HashMap<>();
+    private static HashMap<String, Component> componentMap = new HashMap<>();
     private static HashSet<String> ids = new HashSet<>();
 
     public static JFrame generateWindow(Element element) {
@@ -21,7 +21,7 @@ public class SwingUiPylon {
         ids.add(element.getId());
         if (element.getChildren() != null) {
             for (var child : element.getChildren()) {
-                var panel = generate(child);
+                var panel = generatePanel(child);
                 windowPanel.add(panel);
             }
         }
@@ -30,7 +30,7 @@ public class SwingUiPylon {
         return window;
     }
 
-    private static JPanel generate(Element element) {
+    private static JPanel generatePanel(Element element) {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         var type = ElementType.valueOfLabel(element.getType());
@@ -38,57 +38,85 @@ public class SwingUiPylon {
             ids.add(element.getId());
             switch (type) {
                 case BUTTON:
-                    JButton button = new JButton();
-                    if (element.getText() != null) {
-                        button.setText(element.getText());
-                    } else {
-                        button.setText(element.getId());
-                    }
-                    panel.add(button);
-                    panel.putClientProperty(element.getId(), button);
+                    panel = generateButton(element);
                     break;
                 case LABEL:
-                    JLabel label = new JLabel();
-                    if (element.getText() != null) {
-                        label.setText(element.getText());
-                    }
-                    panel.add(label);
-                    panel.putClientProperty(element.getId(), label);
+                    panel = generateLabel(element);
                     break;
                 case FORM:
-                    JTextArea form = new JTextArea();
-                    if (element.getText() != null) {
-                        form.setText(element.getText());
-                    }
-
-                    // hack for a fixed size of form
-                    form.setMaximumSize(new Dimension(100, 40));
-                    form.setMinimumSize(new Dimension(100, 40));
-
-                    panel.add(form);
-                    panel.putClientProperty(element.getId(), form);
+                    panel = generateForm(element);
                     break;
                 case BLOCK:
                     JPanel subPanel = new JPanel();
                     subPanel.setLayout(new BoxLayout(subPanel, BoxLayout.X_AXIS));
                     if (element.getChildren() != null) {
                         for (var child : element.getChildren()) {
-                            subPanel.add(generate(child));
+                            subPanel.add(generatePanel(child));
                         }
                     }
                     panel.add(subPanel);
                     panel.putClientProperty(element.getId(), subPanel);
                     break;
                 case SPINNER:
-                    JSpinner spinner = new JSpinner();
-                    spinner.setValue(0);
-                    spinner.setMaximumSize(new Dimension(100, 40));
-                    spinner.setMinimumSize(new Dimension(100, 40));
-                    panel.add(spinner);
-                    panel.putClientProperty(element.getId(), spinner);
+                    panel = generateSpinner(element);
                     break;
             }
         }
+        return panel;
+    }
+
+    private static JPanel generateButton(Element element) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JButton button = new JButton();
+        if (element.getText() != null) {
+            button.setText(element.getText());
+        } else {
+            button.setText(element.getId());
+        }
+        panel.add(button);
+        panel.putClientProperty(element.getId(), button);
+        return panel;
+    }
+
+    private static JPanel generateLabel(Element element) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JLabel label = new JLabel();
+        if (element.getText() != null) {
+            label.setText(element.getText());
+        }
+        panel.add(label);
+        panel.putClientProperty(element.getId(), label);
+        return panel;
+    }
+
+    private static JPanel generateForm(Element element) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JTextArea form = new JTextArea();
+        if (element.getText() != null) {
+            form.setText(element.getText());
+        }
+
+        // hack for a fixed size of form
+        form.setMaximumSize(new Dimension(100, 40));
+        form.setMinimumSize(new Dimension(100, 40));
+
+        panel.add(form);
+        panel.putClientProperty(element.getId(), form);
+        return panel;
+    }
+
+    private static JPanel generateSpinner(Element element) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JSpinner spinner = new JSpinner();
+        spinner.setValue(0);
+        spinner.setMaximumSize(new Dimension(100, 40));
+        spinner.setMinimumSize(new Dimension(100, 40));
+        panel.add(spinner);
+        panel.putClientProperty(element.getId(), spinner);
         return panel;
     }
 
