6a9b9fdb20cc96b8a31db27f87898ce6ad0bb62d
Florin Mihalache <florinrmfm3@gmail.com>
2020-04-09 20:46:35 +0300

    Added basic deployment for agent for GUI

--- src/interfaceGenerator/AgentGUI.java
+++ src/interfaceGenerator/AgentGUI.java
@@ -1,14 +1,40 @@
 package interfaceGenerator;
 
+import interfaceGeneratorTest.BuildPageTest;
 import net.xqhs.flash.core.Entity;
 import net.xqhs.flash.core.agent.Agent;
 import net.xqhs.flash.core.support.Pylon;
+import net.xqhs.flash.core.util.MultiTreeMap;
 
 public class AgentGUI implements Agent {
     private static final String name = "AgentGUI";
+    private String interfaceConfiguration;
+
+    public AgentGUI(MultiTreeMap configuration) {
+        // System.out.println(configuration);
+        var guiShard = configuration.getSingleTree("shard");
+        // System.out.println(guiShard);
+        var guiTrees = guiShard.getTrees("gui");
+        // System.out.println(guiTrees.get(0).getTreeKeys());
+        // System.out.println(guiTrees.get(0).getSingleTree("config").getHierarchicalNames());
+        // System.out.println(guiTrees.get(0).getSingleTree("config").getHierarchicalNames().get(0));
+        interfaceConfiguration = guiTrees.get(0).getSingleTree("config").getHierarchicalNames().get(0);
+    }
 
     @Override
     public boolean start() {
+        String[] arguments = new String[2];
+        if (interfaceConfiguration.indexOf('{') != -1) {
+            arguments[0] = BuildPageTest.INLINE;
+        } else {
+            arguments[0] = BuildPageTest.FILE;
+        }
+        arguments[1] = interfaceConfiguration;
+        try {
+            BuildPageTest.main(arguments);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
--- src/interfaceGeneratorTest/DeploymentPageTest.java
+++ src/interfaceGeneratorTest/DeploymentPageTest.java
@@ -2,17 +2,37 @@ package interfaceGeneratorTest;
 
 import net.xqhs.flash.FlashBoot;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class DeploymentPageTest {
     public static void main(String[] args) {
+
         if (args.length == 0) {
+            // YAML configuration provided in command line args
             System.err.println("No args provided");
             return;
         }
 
         String test_args = "";
-        test_args += " -agent AgentA -shard gui config";
-        test_args += " " + args[0];
 
-        FlashBoot.main(test_args.split(" "));
+        test_args += " -package interfaceGenerator";
+        test_args += " -agent AgentA classpath:interfaceGenerator.AgentGUI";
+        test_args += " -shard gui";
+        test_args += " -config";
+
+        var args_list = new ArrayList<String>(Arrays.asList(test_args.split(" ")));
+
+        StringBuilder configuration = new StringBuilder();
+        for (int i = 0; i < args.length; i++) {
+            configuration.append(args[i]);
+            if (i != args.length - 1) {
+                configuration.append(" ");
+            }
+        }
+
+        args_list.add(configuration.toString());
+
+        FlashBoot.main(args_list.toArray(new String[0]));
     }
 }
