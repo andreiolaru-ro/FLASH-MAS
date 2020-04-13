cecb32748cbcb83086d5c129d401157b92804598
Florin Mihalache <florinrmfm3@gmail.com>
2020-04-13 22:15:47 +0300

    Added page creation functionality in GuiShard

--- src/interfaceGenerator/GuiShard.java
+++ src/interfaceGenerator/GuiShard.java
@@ -1,5 +1,7 @@
 package interfaceGenerator;
 
+import interfaceGeneratorTest.BuildPageTest;
+import net.xqhs.flash.core.agent.AgentEvent;
 import net.xqhs.flash.core.shard.AgentShardCore;
 import net.xqhs.flash.core.shard.AgentShardDesignation;
 
@@ -12,4 +14,24 @@ public class GuiShard extends AgentShardCore {
         super(AgentShardDesignation.autoDesignation("GUI"));
     }
 
+    @Override
+    public void signalAgentEvent(AgentEvent event) {
+        super.signalAgentEvent(event);
+        var guiShardConfiguration = super.getShardData().getSingleTree("config");
+        var configuration = guiShardConfiguration.getTreeKeys().get(0);
+
+        String[] parameters = new String[2];
+        parameters[1] = configuration;
+        if (configuration.indexOf('{') == -1) {
+            parameters[0] = BuildPageTest.FILE;
+        } else {
+            parameters[0] = BuildPageTest.INLINE;
+        }
+
+        try {
+            BuildPageTest.main(parameters);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
