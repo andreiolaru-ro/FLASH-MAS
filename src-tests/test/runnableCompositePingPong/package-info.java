5621ed71a043187decc05be38ec1a3e95eba78d9
Andrei Olaru <cs@andreiolaru.ro>
2020-04-14 19:29:30 +0300

    Additional corrections in other tests.

--- src-tests/test/runnableCompositePingPong/package-info.java
+++ src-tests/test/runnableCompositePingPong/package-info.java
@@ -1,4 +1,11 @@
 /**
  * Same example as in compositePingPong, but using the Runnable property of composite agents.
+ * <p>
+ * <b>Verifies:</b>
+ * <ul>
+ * <li>correct access to the thread on which {@link net.xqhs.flash.core.composite.CompositeAgent}s run.
+ * <li>auxiliary: the possibility of different {@link net.xqhs.flash.core.node.Node} implementation and access to
+ * entities inside it.
+ * </ul>
  */
 package test.runnableCompositePingPong;
