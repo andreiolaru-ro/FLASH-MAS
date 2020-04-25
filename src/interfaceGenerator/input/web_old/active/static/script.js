9cc1d74260d695cbfd2e3630c5fccb5b4fb6167d
Florin Mihalache <florinrmfm3@gmail.com>
2020-04-25 22:48:48 +0300

    Deleted old files, AgentWave generated for web active input

--- src/interfaceGenerator/input/web_old/active/static/script.js
+++
@@ -1,5 +0,0 @@
-function getFormText() {
-    var text = document.getElementById("active_input").value;
-    alert(text);
-    return text;
-}
--- src/interfaceGenerator/input/web_old/pasive/static/script.js
+++
@@ -1,5 +0,0 @@
-function getSpinnerValue() {
-    var text = document.getElementById("pasive_input").value;
-    // alert(text);
-    return text;
-}
