9ffad8851105345731d5ccb5d32c2c350f2d9148
Florin Mihalache <florinrmfm3@gmail.com>
2020-04-25 22:39:44 +0300

    No problems with undefined value for input value (web)

--- interface-files/generated-web-pages/js/client.js
+++ interface-files/generated-web-pages/js/client.js
@@ -1,24 +1,24 @@
-var eb;
-var agents_index = {};
-var active_input_value;
-var value;
+let eb;
+let value;
 
 function init() {
     eb = new EventBus("/eventbus");
 
     eb.onopen = () => {
         console.log('Eventbus opened');
+        eb.send('client-to-server', "init");
         eb.registerHandler('server-to-client', (error, message) => {
             console.log("id input from server " + message.body);
             value = document.getElementById(message.body).value;
-            // console.log(value);
+            console.log(value);
+            console.log("value = " + value);
+            eb.send('client-to-server', "active-value: " + value); // sending input value to server
         });
-        eb.send('client-to-server', "init");
     };
 
     eb.onclose = () => {
         console.log('Eventbus closed')
-    }
+    };
 }
 
 function stop() {
@@ -29,15 +29,4 @@ function send_data(id) {
     console.log("Sending data to server");
     console.log(id);
     eb.send('client-to-server', "button-id: " + id); // sending button id to server
-    // receiving form id
-
-    /*
-    eb.registerHandler('server-to-client', (error, message) => {
-        console.log("id input from server " + message.body);
-        value = document.getElementById(message.body).value;
-        console.log(value);
-
-    });
-    */
-    eb.send('client-to-server', "active-value: " + value);
 }
