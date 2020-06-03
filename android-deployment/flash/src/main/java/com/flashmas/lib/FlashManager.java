ebbb50fea3ce48786d254127c2004ad09ee658dd
Daniel <daniel.berbece97@gmail.com>
2020-06-03 17:15:17 +0300

    Move node object to foreground service

--- android-deployment/flash/src/main/java/com/flashmas/lib/FlashManager.java
+++ android-deployment/flash/src/main/java/com/flashmas/lib/FlashManager.java
@@ -8,30 +8,18 @@ import android.view.View;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Observer;
 
 import com.flashmas.lib.agents.gui.AndroidGuiShard;
 
-import net.xqhs.flash.core.DeploymentConfiguration;
 import net.xqhs.flash.core.agent.Agent;
 import net.xqhs.flash.core.composite.CompositeAgent;
-import net.xqhs.flash.core.node.Node;
 import net.xqhs.flash.core.shard.AgentShard;
 import net.xqhs.flash.core.shard.AgentShardDesignation;
 import net.xqhs.flash.core.shard.ShardContainer;
-import net.xqhs.flash.core.support.Pylon;
-import net.xqhs.flash.core.util.MultiTreeMap;
-import net.xqhs.flash.local.LocalSupport;
-import net.xqhs.util.logging.LoggerSimple;
-import net.xqhs.util.logging.logging.Logging;
-import net.xqhs.util.logging.wrappers.GlobalLogWrapper;
 
-import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.flashmas.lib.Globals.NODE_NAME;
 
 /**
  * Singleton class for usage of Flash Framework on Android platform
@@ -40,37 +28,14 @@ public class FlashManager {
     public static final String TAG = FlashManager.class.getSimpleName();
     private static FlashManager instance;
     private static Context appContext;
-    private Node deviceNode;
-    private Pylon devicePylon;
     private MutableLiveData<List<Agent>> agentsLiveData = new MutableLiveData<>();
     private List<Agent> agentsList = new ArrayList<>(0);
-    private static OutputStream logsOutputStream = new ByteArrayOutputStream();
-
-    private String deviceNodeName = NODE_NAME;  // init deviceNodeName with default
-
-    private Observer<Boolean> stateObserver = new Observer<Boolean>() {
-        @Override
-        public void onChanged(Boolean state) {
-            updateAgents();
-        }
-    };
 
 
     private FlashManager() throws IllegalStateException {
         if (appContext == null) {
             throw new IllegalStateException("FlashManager not initialized with application context");
         }
-        NodeForegroundService.isRunningLiveData().observeForever(stateObserver);
-
-        //TODO get unique deviceNodeName in FLASHMAS topology
-        MultiTreeMap nodeConfig = new MultiTreeMap();
-        nodeConfig.add(DeploymentConfiguration.NAME_ATTRIBUTE_NAME, deviceNodeName);
-        deviceNode = new Node(nodeConfig);
-        deviceNode.setLogLevel(LoggerSimple.Level.ALL);
-        devicePylon = new LocalSupport();
-        deviceNode.registerEntity("pylon", devicePylon, devicePylon.getName());
-        GlobalLogWrapper.setLogStream(logsOutputStream);
-        Logging.getMasterLogging().setLogLevel(LoggerSimple.Level.ALL);
     }
 
     public static void init(Context context) {
@@ -93,14 +58,9 @@ public class FlashManager {
         return instance;
     }
 
-    public static OutputStream getLogOutputStream() {
-        return logsOutputStream;
+    public OutputStream getLogOutputStream() {
+        return NodeForegroundService.logsOutputStream;
     }
-
-    Node getDeviceNode() {
-        return deviceNode;
-    }
-
 
     public void startNode() {
         Intent intent = new Intent(appContext, NodeForegroundService.class);
@@ -127,27 +87,22 @@ public class FlashManager {
         if (agent == null)
             return;
 
-        agent.addContext(devicePylon.<Pylon>asContext());
-
-        if (NodeForegroundService.isRunning()) {
-            agent.start();
-        }
-
-        deviceNode.registerEntity("agent", agent, agent.getName());
+        agentsList.add(agent);
         updateAgents();
     }
 
     public void removeAgent(Agent agent) {
-        if (agent == null)
-            return;
-
-        if (agent.isRunning()) {
-            agent.stop();
-        }
-
-        // TODO deregister agent from node
-        // deviceNode.deregisterEntity(...);
-        updateAgents();
+        // TODO
+//        if (agent == null)
+//            return;
+//
+//        if (agent.isRunning()) {
+//            agent.stop();
+//        }
+//
+//        // TODO deregister agent from node
+//        // deviceNode.deregisterEntity(...);
+//        updateAgents();
     }
 
     public LiveData<Boolean> getRunningLiveData() {
@@ -167,7 +122,6 @@ public class FlashManager {
     }
 
     private void updateAgents() {
-        agentsList = deviceNode.getAgentsList();
         agentsLiveData.postValue(agentsList);
     }
 
