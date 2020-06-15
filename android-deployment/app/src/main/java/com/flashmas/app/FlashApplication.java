bd68748e928cb13f3bab5a1d01d43e102fc89617
Daniel <daniel.berbece97@gmail.com>
2020-06-15 17:13:32 +0300

    Add usage example

--- android-deployment/app/src/main/java/com/flashmas/app/FlashApplication.java
+++ android-deployment/app/src/main/java/com/flashmas/app/FlashApplication.java
@@ -1,8 +1,10 @@
 package com.flashmas.app;
 
 import android.app.Application;
+import android.hardware.Sensor;
 
 import com.flashmas.lib.FlashManager;
+import com.flashmas.lib.agents.sensors.SensorsShard;
 
 public class FlashApplication extends Application {
     @Override
@@ -12,17 +14,51 @@ public class FlashApplication extends Application {
         String arg = "-package com.flashmas.lib.agents.gui " +
                 "com.flashmas.lib.agents.sensors" +
                 " -loader agent:composite -node A -pylon webSocket:slave1 connectTo:ws://192.168.0.172:" + port +
-//                " -agent composite:Agent0 -shard messaging -shard SensorsShard SENSOR_TYPES_ARRAY_KEY:" + Sensor.TYPE_ALL + " -shard AndroidGuiShard" +
-//                " -agent composite:Agent1 -shard messaging -shard SensorsShard SENSOR_TYPES_ARRAY_KEY:" + Sensor.TYPE_ALL + " -shard AndroidGuiShard" +
-//                " -agent composite:Agent2 -shard messaging -shard SensorsShard SENSOR_TYPES_ARRAY_KEY:" + Sensor.TYPE_ALL + " -shard AndroidGuiShard" +
-//                " -agent composite:Agent3 -shard messaging -shard SensorsShard SENSOR_TYPES_ARRAY_KEY:" + Sensor.TYPE_ALL + " -shard AndroidGuiShard" +
-//                " -agent composite:Agent4 -shard messaging -shard SensorsShard SENSOR_TYPES_ARRAY_KEY:" + Sensor.TYPE_ALL + " -shard AndroidGuiShard";
+//                " -agent composite:Agent0 -shard messaging -shard SensorsShard SENSOR_TYPES_ARRAY_KEY:" + Sensor.TYPE_ALL +
+//                    " -shard AndroidGuiShard -shard GuiLinkShard SHARD_DESIGNATIONS_KEY:" + SensorsShard.DESIGNATION +
+//
+//                " -agent composite:Agent1 -shard messaging -shard SensorsShard SENSOR_TYPES_ARRAY_KEY:" + Sensor.TYPE_ALL +
+//                " -shard AndroidGuiShard -shard GuiLinkShard SHARD_DESIGNATIONS_KEY:" + SensorsShard.DESIGNATION +
+//
+//                " -agent composite:Agent2 -shard messaging -shard SensorsShard SENSOR_TYPES_ARRAY_KEY:" + Sensor.TYPE_ALL +
+//                " -shard AndroidGuiShard -shard GuiLinkShard SHARD_DESIGNATIONS_KEY:" + SensorsShard.DESIGNATION +
+//
+//                " -agent composite:Agent3 -shard messaging -shard SensorsShard SENSOR_TYPES_ARRAY_KEY:" + Sensor.TYPE_ALL +
+//                " -shard AndroidGuiShard -shard GuiLinkShard SHARD_DESIGNATIONS_KEY:" + SensorsShard.DESIGNATION +
+//
+//                " -agent composite:Agent4 -shard messaging -shard SensorsShard SENSOR_TYPES_ARRAY_KEY:" + Sensor.TYPE_ALL +
+//                " -shard AndroidGuiShard -shard GuiLinkShard SHARD_DESIGNATIONS_KEY:" + SensorsShard.DESIGNATION +
+//
+//                " -agent composite:Agent5 -shard messaging -shard SensorsShard SENSOR_TYPES_ARRAY_KEY:" + Sensor.TYPE_ALL +
+//                " -shard AndroidGuiShard -shard GuiLinkShard SHARD_DESIGNATIONS_KEY:" + SensorsShard.DESIGNATION +
+//
+//                " -agent composite:Agent6 -shard messaging -shard SensorsShard SENSOR_TYPES_ARRAY_KEY:" + Sensor.TYPE_ALL +
+//                " -shard AndroidGuiShard -shard GuiLinkShard SHARD_DESIGNATIONS_KEY:" + SensorsShard.DESIGNATION +
+//
+//                " -agent composite:Agent7 -shard messaging -shard SensorsShard SENSOR_TYPES_ARRAY_KEY:" + Sensor.TYPE_ALL +
+//                " -shard AndroidGuiShard -shard GuiLinkShard SHARD_DESIGNATIONS_KEY:" + SensorsShard.DESIGNATION +
+//
+//                " -agent composite:Agent8 -shard messaging -shard SensorsShard SENSOR_TYPES_ARRAY_KEY:" + Sensor.TYPE_ALL +
+//                " -shard AndroidGuiShard -shard GuiLinkShard SHARD_DESIGNATIONS_KEY:" + SensorsShard.DESIGNATION +
+//
+//                " -agent composite:Agent9 -shard messaging -shard SensorsShard SENSOR_TYPES_ARRAY_KEY:" + Sensor.TYPE_ALL +
+//                " -shard AndroidGuiShard -shard GuiLinkShard SHARD_DESIGNATIONS_KEY:" + SensorsShard.DESIGNATION ;
+                " -agent composite:Agent1 -shard messaging -shard SensorsShard "+SensorsShard.SENSOR_TYPES_ARRAY_KEY+":" + Sensor.TYPE_ALL + " -shard AndroidGuiShard" +
+                " -agent composite:Agent2 -shard messaging -shard SensorsShard "+SensorsShard.SENSOR_TYPES_ARRAY_KEY+":" + Sensor.TYPE_ALL + " -shard AndroidGuiShard" +
+                " -agent composite:Agent3 -shard messaging -shard SensorsShard "+SensorsShard.SENSOR_TYPES_ARRAY_KEY+":" + Sensor.TYPE_ALL + " -shard AndroidGuiShard" +
+                " -agent composite:Agent4 -shard messaging -shard SensorsShard "+SensorsShard.SENSOR_TYPES_ARRAY_KEY+":" + Sensor.TYPE_ALL + " -shard AndroidGuiShard" +
+                " -agent composite:Agent5 -shard messaging -shard SensorsShard "+SensorsShard.SENSOR_TYPES_ARRAY_KEY+":" + Sensor.TYPE_ALL + " -shard AndroidGuiShard" +
+                " -agent composite:Agent6 -shard messaging -shard SensorsShard "+SensorsShard.SENSOR_TYPES_ARRAY_KEY+":" + Sensor.TYPE_ALL + " -shard AndroidGuiShard" +
+                " -agent composite:Agent7 -shard messaging -shard SensorsShard "+SensorsShard.SENSOR_TYPES_ARRAY_KEY+":" + Sensor.TYPE_ALL + " -shard AndroidGuiShard" +
+                " -agent composite:Agent8 -shard messaging -shard SensorsShard "+SensorsShard.SENSOR_TYPES_ARRAY_KEY+":" + Sensor.TYPE_ALL + " -shard AndroidGuiShard" +
+                " -agent composite:Agent9 -shard messaging -shard SensorsShard "+SensorsShard.SENSOR_TYPES_ARRAY_KEY+":" + Sensor.TYPE_ALL + " -shard AndroidGuiShard" +
+                " -agent composite:Agent10 -shard messaging -shard SensorsShard "+SensorsShard.SENSOR_TYPES_ARRAY_KEY+":" + Sensor.TYPE_ALL + " -shard AndroidGuiShard";
 
-                " -agent composite:Agent0 -shard messaging -shard AndroidGuiShard" +
-                " -agent composite:Agent1 -shard messaging -shard AndroidGuiShard" +
-                " -agent composite:Agent2 -shard messaging -shard AndroidGuiShard" +
-                " -agent composite:Agent3 -shard messaging -shard AndroidGuiShard" +
-                " -agent composite:Agent4 -shard messaging -shard AndroidGuiShard";
+//                " -agent composite:Agent0 -shard messaging -shard AndroidGuiShard" +
+//                " -agent composite:Agent1 -shard messaging -shard AndroidGuiShard" +
+//                " -agent composite:Agent2 -shard messaging -shard AndroidGuiShard" +
+//                " -agent composite:Agent3 -shard messaging -shard AndroidGuiShard" +
+//                " -agent composite:Agent4 -shard messaging -shard AndroidGuiShard";
         FlashManager.init(this, arg);
     }
 }
