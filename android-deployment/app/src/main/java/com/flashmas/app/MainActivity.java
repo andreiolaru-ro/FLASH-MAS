a7bfa7fbcf8220eb7a5b191801382c43ea55dc25
Daniel <daniel.berbece97@gmail.com>
2020-06-15 17:09:09 +0300

    Allow overriding of getAgent method for activities extending FlashActivity

--- android-deployment/app/src/main/java/com/flashmas/app/MainActivity.java
+++ android-deployment/app/src/main/java/com/flashmas/app/MainActivity.java
@@ -1,12 +1,28 @@
 package com.flashmas.app;
 
+import android.hardware.Sensor;
 import android.os.Bundle;
 
+import com.flashmas.lib.FlashManager;
+import com.flashmas.lib.agents.sensors.SensorsShard;
 import com.flashmas.lib.ui.FlashActivity;
+
+import net.xqhs.flash.core.agent.Agent;
+
+import java.util.Arrays;
 
 public class MainActivity extends FlashActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected Agent getCompositeAgent() {
+        return FlashManager.getCompositeAgentBuilder()
+                .addGuiShard()
+                .addSensorShard(Arrays.asList(Sensor.TYPE_ALL))
+                .addGuiLinkShard(Arrays.asList(SensorsShard.DESIGNATION))
+                .build();
     }
 }
