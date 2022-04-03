c2dde9c47ed36bc9ef20f23ab562bc9b3162decd
Andrei Olaru <cs@andreiolaru.ro>
2022-04-03 17:35:54 +0300

    Promoted the guiWorks test as an official example.

--- src-examples/example/guiGeneration/BootGuiAgent.java
+++ src-examples/example/guiGeneration/BootGuiAgent.java
@@ -9,7 +9,7 @@
  * 
  * You should have received a copy of the GNU General Public License along with Flash-MAS.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
-package andrei.guiWorks;
+package example.guiGeneration;
 
 import net.xqhs.flash.FlashBoot;
 
