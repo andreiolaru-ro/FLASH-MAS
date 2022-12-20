4b5ce18657ac69c0f82f56e9bdb9147939954301
andreiolaru <andreiolaru@Comp9m>
2022-12-20 10:21:49 +0200

    Picked modifications from the bin testing branch.

--- src-testing/JadeScript/MessagingAgent.java
+++ src-testing/JadeScript/MessagingAgent.java
@@ -25,7 +25,12 @@ public class MessagingAgent extends Agent {
 					ACLMessage msg1 = new ACLMessage(ACLMessage.INFORM);
 					for(int i = 3; i < 17; i += 2)
 						msg1.addReceiver(new AID(Integer.valueOf(i).toString(), AID.ISLOCALNAME));
-					msg1.setContent("start");
+					// variant for LauncherA
+					// for(int i = 2; i <= 4; i += 1)
+					// msg1.addReceiver(new AID(Integer.valueOf(i).toString(), AID.ISLOCALNAME));
+					// for(int i = 13; i <= 16; i += 1)
+					// msg1.addReceiver(new AID(Integer.valueOf(i).toString(), AID.ISLOCALNAME));
+					// msg1.setContent("start");
 					send(msg1);
 				}
 				
