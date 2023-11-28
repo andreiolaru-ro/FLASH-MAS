00073f4e2ccd338abfdad73af21be9f2c318c17f
Alexandru Sorici <alex.sorici@gmail.com>
2023-11-28 15:13:01 +0200

    Adding examples of ontology driver usage in MLManagementShard

--- src/aifolk/core/MLManagementShard.java
+++ src/aifolk/core/MLManagementShard.java
@@ -4,6 +4,7 @@
 package aifolk.core;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
@@ -13,7 +14,15 @@ import com.google.gson.JsonParser;
 import aifolk.onto.OntologyDriver;
 import aifolk.onto.Query;
 import aifolk.onto.QueryResult;
+import aifolk.onto.vocab.CoreVocabulary;
+import aifolk.onto.vocab.DrivingSceneContextDescription;
+import aifolk.onto.vocab.ExportableDescription;
+import aifolk.onto.vocab.ExtractableDescription;
+import aifolk.onto.vocab.ModelDescription;
+import aifolk.onto.vocab.TaskDescription;
 import aifolk.scenario.ScenarioShard;
+import fr.inria.corese.core.Graph;
+import fr.inria.corese.core.load.LoadException;
 import net.xqhs.flash.core.Entity;
 import net.xqhs.flash.core.agent.AgentEvent;
 import net.xqhs.flash.core.agent.AgentEvent.AgentEventType;
@@ -52,7 +61,7 @@ public class MLManagementShard extends AgentShardGeneral {
 	}
 	
 	@Override
-	public boolean addGeneralContext(EntityProxy<? extends Entity<?>> context) {
+	public boolean addGeneralContext(final EntityProxy<? extends Entity<?>> context) {
 		if(!super.addGeneralContext(context))
 			return false;
 		if(context instanceof MLDriver) {
@@ -69,7 +78,7 @@ public class MLManagementShard extends AgentShardGeneral {
 	}
 	
 	@Override
-	public void signalAgentEvent(AgentEvent event) {
+	public void signalAgentEvent(final AgentEvent event) {
 		super.signalAgentEvent(event);
 		// TODO if event is output from agent ML pipeline, evaluate and decide
 		
@@ -82,23 +91,52 @@ public class MLManagementShard extends AgentShardGeneral {
 			
 			// situation element: no of pedestrians
 			li("Result:", mlDriver.predict("YOLOv8-pedestrians", (String) event.getObject("input"), false));
+			final DrivingSceneContextDescription.Builder dataContextBuilder = DrivingSceneContextDescription.Builder.create("http://aimas.cs.pub.ro/ai-folk/ontology/drivingSegmentation#my_data_context_1");
+			dataContextBuilder.withAvgNumPedestrians(150);
+			final DrivingSceneContextDescription dataContextDesc = dataContextBuilder.build();
+			
+			final TaskDescription.Builder taskBuilder = TaskDescription.Builder.create("http://aimas.cs.pub.ro/ai-folk/ontology/drivingSegmentation#my_task_1", 
+										"http://aimas.cs.pub.ro/ai-folk/ontology/core#SemanticSegmentation");
+			taskBuilder.setDomainURI("http://aimas.cs.pub.ro/ai-folk/ontology/drivingSegmentation#AutonomousDriving");
+			taskBuilder.addDataContextDescription(dataContextDesc);
+			final TaskDescription taskDesc = taskBuilder.build();
+
+			// TODO send task description to other agents by first getting a String serialization of it
+			final Optional<String> taskDescStr = ExportableDescription.graphToString(taskDesc.exportToGraph());
+
+			// Parse the serialized task description to obtain a task description object
+			try {
+				final Graph taskDescGraph = ExtractableDescription.getGraphFromString(taskDescStr.get());
+				final String taskDescInstanceURI = ExtractableDescription.getSingleConceptURI(taskDescGraph, CoreVocabulary.TASK_CHARACTERIZATION.stringValue());
+				final TaskDescription extractedTaskDesc = new TaskDescription(taskDescGraph, taskDescInstanceURI);
+				extractedTaskDesc.populateDescription(false);
+
+				final ModelDescription modelDesc = ModelDescription.getFromFile("");
+				modelDesc.populateDescription(true);
+				((DrivingSceneContextDescription)modelDesc.getModelEvaluations().get(0).getDatasetDescription().getDataContextDescriptions().get(0)).getAvgNumPedestrians();
+
+			} catch (final LoadException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 			// TODO match with current model description
 			// TODO if insufficient match, build query
 			// TODO send query to other agents
 		}
 		else {
-			List<String> sources = event.getValues(AgentWave.SOURCE_ELEMENT);
+			final List<String> sources = event.getValues(AgentWave.SOURCE_ELEMENT);
 			if(sources.size() > 2 && !getAgent().getEntityName().equals(sources.get(0))
 					&& DESIGNATION.equals(sources.get(1))) {
 				// wave is from the MLManagementShard of a different agent
-				JsonObject jsonObj = JsonParser.parseString(event.get(AgentWave.CONTENT)).getAsJsonObject();
-				String messageType = jsonObj.get(AIFolkProtocol.FOLK_PROTOCOL).getAsString();
-				JsonArray arguments = jsonObj.get(AIFolkProtocol.FOLK_ARGUMENTS).getAsJsonArray();
+				final JsonObject jsonObj = JsonParser.parseString(event.get(AgentWave.CONTENT)).getAsJsonObject();
+				final String messageType = jsonObj.get(AIFolkProtocol.FOLK_PROTOCOL).getAsString();
+				final JsonArray arguments = jsonObj.get(AIFolkProtocol.FOLK_ARGUMENTS).getAsJsonArray();
 				switch(messageType) {
 				case AIFolkProtocol.FOLK_SEARCH:
-					String queryString = arguments.get(0).getAsString();
-					QueryResult[] results = ontDriver.runQuery(new Query(queryString));
-					AgentWave reply = ((AgentWave) event).createReply(new Gson().toJson(results));
+					final String queryString = arguments.get(0).getAsString();
+					final QueryResult[] results = ontDriver.runQuery(new Query(queryString));
+					final AgentWave reply = ((AgentWave) event).createReply(new Gson().toJson(results));
 					sendMessage(reply);
 					break;
 				// TODO
