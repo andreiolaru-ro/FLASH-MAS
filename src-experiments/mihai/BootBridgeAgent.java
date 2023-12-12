1f01318372dda802b0cf50c7c946735695f5076d
Andrei Olaru <cs@andreiolaru.ro>
2023-12-12 23:53:21 +0200

    Reverted WSRegions testing script.
    Fixed Mihai's script and added exit times (although the platform as a
    whole does not exit)

--- src-experiments/mihai/BootBridgeAgent.java
+++ src-experiments/mihai/BootBridgeAgent.java
@@ -1,7 +1,6 @@
 package mihai;
 
 import net.xqhs.flash.FlashBoot;
-import net.xqhs.util.logging.MasterLog;
 
 /**
  * Deployment testing.
@@ -10,32 +9,36 @@ public class BootBridgeAgent {
 	/**
 	 * Performs test
 	 * 
-	 * @param args_ - not used.
+	 * @param args_
+	 *            - not used.
 	 */
 	public static void main(String[] args_) {
 		String args = "";
-
-		args += " -package testing wsRegions src-tests.test.wsRegionsDeployment.Script -loader agent:mobileComposite";
-
+		
+		args += " -package testing wsRegions src-experiments.mihai -loader agent:mobileComposite";
+		
 		args += " -node nodeA-localhost:8885";
 		args += " -pylon WSRegions:pylonA isServer:localhost:8885";
-		args += " -agent :agentA-localhost:8885 -shard messaging  -shard EchoTesting -shard ScriptTesting from:Simple";
-
+		// args += " -agent :agentA-localhost:8885 -shard messaging -shard EchoTesting -shard PingTest
+		// otherAgent:agentD-localhost:8885";
+		args += " -agent :agentA-localhost:8885 -shard messaging  -shard EchoTesting exit:20 -shard ScriptTesting from:Simple";
+		
 		args += " -node nodeB-localhost:8885";
 		args += " -pylon WSRegions:pylonB1 connectTo:localhost:8885";
-//		args += " -pylon webSocket:pylonB2 serverPort:8886";
-		args += " -agent :agentB-localhost:8885 -shard messaging -shard EchoTesting";
-
+		// args += " -pylon webSocket:pylonB2 serverPort:8886";
+		args += " -agent :agentB-localhost:8885 -shard messaging -shard EchoTesting exit:20";
+		
 		args += " -node nodeC-localhost:8885";
 		args += " -pylon WSRegions:pylonC connectTo:localhost:8885";
-		args += " -agent :agentC-localhost:8885 -shard messaging -shard EchoTesting";
-
+		args += " -agent :agentC-localhost:8885 -shard messaging -shard EchoTesting exit:20";
+		
 		args += " -node nodeD-localhost:8885";
 		args += " -pylon WSRegions:pylonD connectTo:localhost:8885";
-		args += " -agent :agentD-localhost:8885 -shard messaging -shard EchoTesting -shard ScriptTesting from:Simple";
-
-		MasterLog.enablePerformanceModeTools(500);
-		System.out.println("."); // to activate console output.
+		// args += " -agent :agentD-localhost:8885 -shard messaging -shard EchoTesting -shard PingBackTest";
+		args += " -agent :agentD-localhost:8885 -shard messaging -shard EchoTesting exit:20 -shard ScriptTesting from:Simple";
+		
+		// MasterLog.enablePerformanceModeTools(500);
+		// System.out.println("."); // to activate console output.
 		FlashBoot.main(args.split(" "));
 	}
 }
