91b55f4b48183f3d2e180d9a7c6b0b8fbe0b3343
Andrei Olaru <cs@andreiolaru.ro>
2024-03-01 11:38:43 +0200

    Small fixes

--- src-experiments/mihai/BootBridgeAgent.java
+++ src-experiments/mihai/BootBridgeAgent.java
@@ -15,20 +15,20 @@ public class BootBridgeAgent {
 	public static void main(String[] args_) {
 		String args = "";
 		
-		args += " -package test.simplePingPong wsRegions";
+		args += " -package test.simplePingPong mihai wsRegions";
 		
 		args += " -node nodeA-localhost:8885";
 		args += " -pylon WSRegions:pylonA1 isServer:localhost:8885";
 		args += " -pylon webSocket:pylonA2 serverPort:8886";
-		args += " -agent agentA-localhost classpath:BridgeAgent ping:agentD pong:agentB-localhost:8885 -shard messaging -shard EchoTesting exit:30";
+		args += " -agent agentA-localhost classpath:BridgeAgent ping:agentD pong:agentB-nodeB-localhost:8885 in-context-of:WSRegions:pylonA1";
 		
 		args += " -node nodeB-localhost:8885";
 		args += " -pylon WSRegions:pylonB connectTo:localhost:8885";
-		args += " -agent composite:agentB-localhost:8885 -shard messaging -shard EchoTesting exit:30";
+		args += " -agent composite:agentB-nodeB-localhost:8885 -shard messaging -shard EchoTesting exit:30";
 		
 		args += " -node nodeD";
 		args += " -pylon webSocket:pylonD connectTo:ws://localhost:8886";
-		args += " -agent agentD classpath:AgentPingPong sendTo:agentA-localhost -shard EchoTesting exit:30";
+		args += " -agent agentD classpath:AgentPingPong sendTo:agentA-localhost";
 		
 		FlashBoot.main(args.split(" "));
 	}
