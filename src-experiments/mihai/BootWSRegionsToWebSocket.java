7264037665d9172ba15a6490a67397d37f2f007c
mihaistoica2205 <mihai.stoica2205@gmail.com>
2024-06-21 12:19:01 +0300

    wsregions test change

--- src-experiments/mihai/BootWSRegionsToWebSocket.java
+++ src-experiments/mihai/BootWSRegionsToWebSocket.java
@@ -15,19 +15,25 @@ public class BootWSRegionsToWebSocket {
 	public static void main(String[] args_) {
 		String args = "";
 		args += " -package wsRegions mihai testing src-tests.test.wsRegionsDeployment.Script -loader agent:composite -load_order pylon;agent;bridge";
-		// TODO: fix Script
-		args += " -node nodeB keep:20";
-		args += " -pylon webSocket:pylonWebsocket isServer:localhost:8886";
-		args += " -pylon WSRegions:pylonWSRegions isServer:localhost:8885";
-		args += " -bridge interoperability:bridge1 in-context-of:webSocket:pylonWebsocket";
 
-		args += " -node nodeA";
-		args += " -pylon WSRegions:pylonA connectTo:localhost:8885";
-		args += " -agent :ws://localhost:8885/agentA -shard messaging -shard ScriptTesting from:Simple";
+		if (args_[0].equals("1")) {
+			args += " -node nodeB keep:20";
+			args += " -pylon webSocket:pylonWebsocket isServer:10.89.51.225:8886";
+			args += " -pylon WSRegions:pylonWSRegions isServer:10.89.51.225:8885";
+			args += " -bridge interoperability:bridge1 in-context-of:webSocket:pylonWebsocket";
+		}
 
-		args += " -node nodeC";
-		args += " -pylon webSocket:pylonC connectTo:ws://localhost:8886";
-		args += " -agent :agentC -shard messaging";
+		if (args_[0].equals("2")) {
+			args += " -node nodeA";
+			args += " -pylon WSRegions:pylonA connectTo:10.89.51.225:8885";
+			args += " -agent :ws://10.89.51.225:8885/agentA -shard messaging -shard ScriptTesting from:Simple";
+		}
+
+		if (args_[0].equals("3")) {
+			args += " -node nodeC";
+			args += " -pylon webSocket:pylonC connectTo:ws://10.89.51.225:8886";
+			args += " -agent :agentC -shard messaging";
+		}
 
 		FlashBoot.main(args.split(" "));
 	}
