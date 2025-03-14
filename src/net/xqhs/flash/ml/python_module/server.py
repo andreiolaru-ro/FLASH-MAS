f6039af9aec8d11422a43e091348366daa52fb9a
Andrei Olaru <cs@andreiolaru.ro>
2025-03-14 15:09:30 +0200

    Introduced temporary testing code

--- src/net/xqhs/flash/ml/python_module/server.py
+++ src/net/xqhs/flash/ml/python_module/server.py
@@ -67,9 +67,28 @@ datasets = model_store.load_datasets_from_config(ML_DIRECTORY_PATH + MODEL_CONFI
 # log("Test exit.")
 # exit(0)
 
+def dotesting():
+    model_name = "YOLOv8-pedestrians"
+    input_data = ML_DIRECTORY_PATH + "input/a31ee6f_ADE_val_00000798.jpg"
+
+    if model_name in models:
+        model = models[model_name]
+        processed_input = model.process_input(input_data)
+        output = model.predict(processed_input)
+        result = model.process_output(output)
+        response = {'prediction': (result if isinstance(result, list) else [result])}
+        ret = response
+        log("returned", ret)
+        return ret
+    else:
+        log("Model not found")
+
+
 if __name__ == '__main__':
     log("starting...")
-    MLServer.start()
+    # MLServer.start()
+    log("testing...")
+    dotesting()    
 else:
     log("nothing to do for now.")
 
