f87b233cc893bb6e71aa93ca7e80afb4ff4cd9aa
donutz03 <109514731+ionuthodoroaga03@users.noreply.github.com>
2026-04-12 14:15:15 +0300

    update WolfSheepBoot

--- src-experiments/abms/wolfSheepPredation/WolfSheepBoot.java
+++ src-experiments/abms/wolfSheepPredation/WolfSheepBoot.java
@@ -3,21 +3,21 @@ package abms.wolfSheepPredation;
 import net.xqhs.flash.FlashBoot;
 
 public class WolfSheepBoot {
-	public static void main(String[] args_) {
-		String a = "";
+    public static void main(String[] args_) {
+        String a = "";
 
-		a += " -load_order simulation;executor;context;WolfSheepGroup";
-		a += " -package net.xqhs.flash.abms";
-		a += " -package abms.wolfSheepPredation";
-		a += " -loader WolfSheepGroup classpath:abms.wolfSheepPredation.WolfSheepGroupLoader";
-		a += " -node dummy -simulation sim classpath:Simulation";
-		a += " -executor StepWise:StepWise steps:100";
+        a += " -load_order simulation;executor;context;WolfSheepGroup";
+        a += " -package net.xqhs.flash.abms";
+        a += " -package abms.wolfSheepPredation";
+        a += " -loader WolfSheepGroup classpath:abms.wolfSheepPredation.WolfSheepGroupLoader";
+        a += " -node dummy -simulation sim classpath:Simulation";
+        a += " -executor StepWise:StepWise steps:100";
         a += " -context AgentManagement:agentManagement";
         a += " -context Random:random seed:42";
         a += " -context ProximityCommunication:communication";
         a += " -context Space:space width:6 height:6";
-		a += " -WolfSheepGroup g sheepCount:10 wolfCount:5";
+        a += " -WolfSheepGroup g -agent Sheep n:10 visionRange:2 -agent Wolf n:5 visionRange:3 -agent Grass n:15";
 
-		FlashBoot.main(a.split(" "));
-	}
+        FlashBoot.main(a.split(" "));
+    }
 }
