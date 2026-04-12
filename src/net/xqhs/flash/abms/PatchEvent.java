f20d64a076f048b4d43f2e64f0e9aca925eef6d9
donutz03 <109514731+ionuthodoroaga03@users.noreply.github.com>
2026-04-12 18:14:09 +0300

    add PatchEvent class

---
+++ src/net/xqhs/flash/abms/PatchEvent.java
@@ -0,0 +1,14 @@
+package net.xqhs.flash.abms;
+
+public class PatchEvent {
+
+    private final String type;
+
+    public PatchEvent(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
