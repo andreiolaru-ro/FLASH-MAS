c9452c48bc385628a7003f6ccb8ad3aa6508f6b8
alexandrulazar1008 <alexandru.lazar1008@stud.acs.upb.ro>
2026-05-09 10:40:28 +0300

    fix: support unnamed entities in CLI deployment

---
+++ src-tests/test/deployment/name/Boot.java
@@ -0,0 +1,32 @@
+package test.deployment.name;
+
+import net.xqhs.flash.FlashBoot;
+
+public class Boot {
+    public static void main(String[] args) {
+
+        String test_args = "-package testing ";
+        // node nu are nume. Este null
+        // se da load la node dar nu poate sa ii dea start
+        //test_args += "-node";
+
+        //pylonul ia numele "-agent"
+        //Agent1 devine un parametru al pylonului cu valoarea null
+        //Could not load entity [-agent]/[#728890494] of type [pylon].
+        //test_args += "-node N1 -pylon -agent Agent1";
+
+        // agentul are numele "classpath:DummyAgent"
+        //Could not load entity [classpath:DummyAgent]/[#728890494] of type [agent].
+        //test_args += "-node N1 -agent classpath:DummyAgent param:val";
+
+        //numele nodului este "-pylon", pylon-ul se creeaza automat default, iar agentul are numele "-shard"
+        //Could not load entity [-shard]/[#728890494] of type [agent].
+        //node loaded: [-pylon]
+        //EchoTesting devine un parametru al agentului cu valoarea null
+        //test_args += "-node -pylon -agent -shard EchoTesting exit:2";
+
+
+        test_args += "-node -pylon -agent - classpath:DummyAgent param:val";
+        FlashBoot.main(test_args.split(" "));
+    }
+}
--- src/net/xqhs/flash/core/DeploymentConfiguration.java
+++ src/net/xqhs/flash/core/DeploymentConfiguration.java
@@ -780,19 +780,23 @@ public class DeploymentConfiguration extends MultiTreeMap {
 		Deque<CtxtTriple> context = new LinkedList<>();
 		if(baseContext != null)
 			context.push(baseContext);
-		while(args.hasNext()) {
+		String lookahead = null;
+		while(lookahead != null || args.hasNext()) {
 			// log.lf(context.toString());
-			String a = args.next();
+			String a;
+			if(lookahead != null) {
+				a = lookahead;
+				lookahead = null;
+			}
+			else {
+				a = args.next();
+			}
 			if(a.trim().length() == 0)
 				continue;
 			if(isCategoryDefinition(a)) {
 				// get category
 				String catName = getCategoryName(a);
 				CategoryName category = CategoryName.byName(getCategoryName(a));
-				if(!args.hasNext()) { // must check this before creating any trees
-					log.lw("Empty category [] in CLI arguments.", catName);
-					return;
-				}
 				
 				// create / find the context
 				// search upwards in the current context for a parent or at least an ancestor
@@ -853,26 +857,39 @@ public class DeploymentConfiguration extends MultiTreeMap {
 				
 				// integrate in current context.
 				CtxtTriple cCtxt = context.peek();
-				if(cCtxt.elemTree == null)
-					log.le("Unable to integrate category [] in current context [] which does not support subordinate categories.",
-							catName, cCtxt.category);
-				else if(category != null && category.isValue()) { // it is a simple value
-					context.push(new CtxtTriple(catName, null, null));
-				}
-				else { // it is an entity; get entity name
-					String name = args.next();
-					MultiTreeMap subCatTree = integrateChildCat(cCtxt.elemTree, catName, log);
-					MultiTreeMap node;
-					if(subCatTree.isHierarchical(name))
-						node = subCatTree.isSingleton(name) ? subCatTree.getSingleTree(name)
-								: subCatTree.getFirstTree(name);
-					else {
-						node = new MultiTreeMap();
-						node.addOneValue(NAME_ATTRIBUTE_NAME, name);
-						integrateName(node, catName, subCatTree, rootTree, autoCreated, name_ids, log);
-					}
-					context.push(new CtxtTriple(catName, subCatTree, node));
-				}
+                if(cCtxt.elemTree == null)
+                    log.le("Unable to integrate category [] in current context [] which does not support subordinate categories.",
+                            catName, cCtxt.category);
+                else if(category != null && category.isValue()) { // it is a simple value
+                    context.push(new CtxtTriple(catName, null, null));
+                }
+                else { // it is an entity; get entity name
+                    String name = null;
+                    if(args.hasNext()) {
+                        name = args.next();
+
+                        if(isCategoryDefinition(name)) {
+                            lookahead = name;
+                            name = null;
+                        } else if("-".equals(name)) {
+                            name = null;
+                        }
+                    }
+
+                    MultiTreeMap subCatTree = integrateChildCat(cCtxt.elemTree, catName, log);
+                    MultiTreeMap node;
+                    if(name != null && subCatTree.isHierarchical(name))
+                        node = subCatTree.isSingleton(name) ? subCatTree.getSingleTree(name)
+                                : subCatTree.getFirstTree(name);
+                    else {
+                        node = new MultiTreeMap();
+                        if(name != null) {
+                            node.addOneValue(NAME_ATTRIBUTE_NAME, name);
+                        }
+                        integrateName(node, catName, subCatTree, rootTree, autoCreated, name_ids, log);
+                    }
+                    context.push(new CtxtTriple(catName, subCatTree, node));
+                }
 			}
 			else {
 				if(context.size() <= 1) {
